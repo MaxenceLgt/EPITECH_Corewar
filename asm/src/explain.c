af448fcd2df2d7fae401dc8f55bb8a498c11a9a4
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-05-15 15:00:11 +0200

    refactor(main): Modify output of usage option and add name to githubactions

--- asm/src/explain.c
+++ asm/src/explain.c
@@ -12,11 +12,11 @@
 void explain_h(char **av)
 {
     if (my_strcmp(av[1], "-h") == 0) {
-        write(2, "USAGE\n", 7);
-        write(2, "./asm file_name[.s]\n", 21);
-        write(2, "DESCRIPTION\n", 13);
-        write(2, "file_name file in assembly ", 27);
-        write(2, "language to be converted into ", 30);
-        write(2, "file_name.cor, an executable in the Virtual Machine.\n", 53);
+        write(1, "USAGE\n", 7);
+        write(1, "./asm file_name[.s]\n", 21);
+        write(1, "DESCRIPTION\n", 13);
+        write(1, "file_name file in assembly ", 27);
+        write(1, "language to be converted into ", 30);
+        write(1, "file_name.cor, an executable in the Virtual Machine.\n", 53);
     }
 }
