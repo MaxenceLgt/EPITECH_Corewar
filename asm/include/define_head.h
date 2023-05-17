b805cd303abce57c7b2b93c85857a8b4d7b269b2
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-05-17 19:30:55 +0200

    feat(max_asm): Add header error handling, get file content function and header use to put define in it

---
+++ asm/include/define_head.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** define_head
+** File description:
+** define_head
+*/
+
+#ifndef DEFINE_HEAD_
+    #define DEFINE_HEAD_
+
+    #define CHAR_IS(c, lst) ml_char_is_in_lst(c, lst) == true
+    #define PASS ml_printf("Je passe par ici\n")
+
+#endif /* !DEFINE_HEAD_ */
