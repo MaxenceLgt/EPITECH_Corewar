6f8ec3e0a8cf424993612b6efd40032c72e87216
mlargeot <morgan.largeot@epitech.eu>
2023-05-22 10:28:07 +0200

    feat(morgan_vm): Add init vm code structure

--- corewar/include/corewar_header.h
+++ corewar/include/corewar_header.h
@@ -8,10 +8,22 @@
 #ifndef COREWAR_HEADER_
     #define COREWAR_HEADER_
 
+    // INCLUDES
+
     #include "ml_lib_head.h"
+
+    // STRUCT
+
+typedef struct virtual_machine {
+    char *tmp;
+} vm_t;
 
     // DOC
 
     void manage_help(int ac, char **av);
+
+    // INIT
+
+    vm_t *init_vm(char **av);
 
 #endif /* !COREWAR_HEADER_ */
---
+++ corewar/src/init/init_vm.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** corewar
+** File description:
+** init_vm
+*/
+
+#include "corewar_header.h"
+
+vm_t *init_vm(char **av)
+{
+    return 0;
+}
