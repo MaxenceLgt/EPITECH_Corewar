7207bd070e7ca4ea657872ba96e5783f9a8c77cf
allain <lea.allain@epitech.eu>
2023-05-23 16:05:19 +0200

    fix(lea): check coding style

--- corewar/include/corewar_header.h
+++ corewar/include/corewar_header.h
@@ -10,11 +10,8 @@
 
     #include "ml_lib_head.h"
 
-    #define CHECK_HEXA (av[2][i] >= '0' && av[2][i] <= '9') \
-|| (av[2][i] >= 65 && av[2][i] <= 70)
-
     void explain_h(char **av);
     int error_handling(char **av, int ac, int size_vm, int tot_vm);
-    int init_flag_n(char **av, int size_vm, int tot_vm);
+    int init_flag_n(int size_vm, int tot_vm);
 
 #endif /* !COREWAR_HEADER_ */
--- corewar/src/error_handling.c
+++ corewar/src/error_handling.c
@@ -8,15 +8,6 @@
 #include "corewar_header.h"
 #include <stdio.h>
 
-static int check_hexa(char **av)
-{
-    for (int i = 0; av[2][i]; i++)
-        if (CHECK_HEXA);
-        else
-            return 84;
-    return 0;
-}
-
 static int check_nb(char *av)
 {
     for (int i = 0; av[i]; i++)
@@ -26,27 +17,20 @@ static int check_nb(char *av)
     return 0;
 }
 
-int error_handling(char **av, int ac, int size_vm, int tot_vm)
+static int check_arg(char **av, int *i, int *count_no_n)
 {
-    int i;
-    int count_no_n = 0;
-
-    for (i = 0; i < ac; i++) {
-        if (ml_strcmp(av[i], "-dump") == 0)
-            if (check_nb(av[i + 1]) == 84) return 84;
-        if (ml_strcmp(av[i], "-n") == 0)
-            if (check_nb(av[i + 1]) == 84) return 84;
-        if (ml_strcmp(av[i], "-a") == 0)
-            if (check_nb(av[i + 1]) == 84) return 84;
-        if (ml_strcmp(av[i], "-n") != 0)
+    if (ml_strcmp(av[*i], "-dump") == 0)
+        if (check_nb(av[*i + 1]) == 84) return 84;
+    if (ml_strcmp(av[*i], "-n") == 0)
+        if (check_nb(av[*i + 1]) == 84) return 84;
+    if (ml_strcmp(av[*i], "-a") == 0)
+        if (check_nb(av[*i + 1]) == 84) return 84;
+    if (ml_strcmp(av[*i], "-n") != 0)
         count_no_n++;
-    }
-    if (count_no_n == ac)
-        init_flag_n(av, size_vm, tot_vm);
     return 0;
 }
 
-int init_flag_n(char **av, int size_vm, int tot_vm)
+int init_flag_n(int size_vm, int tot_vm)
 {
     int distance_vm;
 
@@ -54,5 +38,17 @@ int init_flag_n(char **av, int size_vm, int tot_vm)
         distance_vm = ((tot_vm - size_vm) / 2) + size_vm;
     else
         distance_vm = ((tot_vm - size_vm) / 2);
+    return distance_vm;
+}
+
+int error_handling(char **av, int ac, int size_vm, int tot_vm)
+{
+    int i;
+    int count_no_n = 0;
+
+    for (i = 0; i < ac; i++)
+        check_arg(av, &i, &count_no_n);
+    if (count_no_n == ac)
+        init_flag_n(size_vm, tot_vm);
     return 0;
 }
