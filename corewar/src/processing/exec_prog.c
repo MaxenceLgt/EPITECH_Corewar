800a753c2ebd9d91fd0ed7b510548c6ed83cb07f
mlargeot <morgan.largeot@epitech.eu>
2023-05-25 17:10:47 +0200

    feat(morgan_vm): Add execution parsing (need to implement op struct  + functions)

--- corewar/src/processing/exec_prog.c
+++ corewar/src/processing/exec_prog.c
@@ -7,7 +7,32 @@
 
 #include "corewar_header.h"
 
+static int exc_function(void)
+{
+    return 0;
+}
+
+static int handle_champ_process(vm_t *vm, champ_t *champ)
+{
+    ml_node *node = champ->process->head;
+    process_t *current_process = NULL;
+
+    for (;node; node = node->next) {
+        current_process = node->data;
+        if (vm->current_cycle = current_process->goal_cycle)
+            exc_funciton();
+    }
+    return 0;
+}
+
 int exec_prog(UNUSED vm_t *vm)
 {
+    ml_node *node = vm->champs_data->head;
+    champ_t *champ = NULL;
+
+    for(; node; node = node->next) {
+        champ = node->data;
+        handle_champ_process(vm, champ);
+    }
     return 0;
 }
