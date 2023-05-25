dd48518b6aef6a752ffaf6fe158e43addaded08e
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-05-25 09:56:03 +0200

    refactor(lea): resolve merge problem

--- corewar/src/tools/traduction.c
+++ corewar/src/tools/traduction.c
@@ -9,8 +9,7 @@
 
 int get_hexa(unsigned char buffer)
 {
-    int nb = buffer;
+    UNUSED int nb = buffer;
 
-    printf("%c%c", "0123456789abcdef"[nb / 16], "0123456789abcdef"[nb % 16]);
     return nb;
 }
