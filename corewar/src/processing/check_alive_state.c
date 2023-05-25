b032b851f919d8e20baab511f2ec45d3f9cda15c
mlargeot <morgan.largeot@epitech.eu>
2023-05-25 16:15:30 +0200

    refactor(morgan_vm): Fix missing lines to check alive champions

--- corewar/src/processing/check_alive_state.c
+++ corewar/src/processing/check_alive_state.c
@@ -12,8 +12,10 @@ int check_alive_state(vm_t *vm)
     ml_node *champ = vm->champs_data->head;
     champ_t *info = NULL;
 
-    if (vm->nbr_live == NBR_LIVE)
+    if (vm->nbr_live == NBR_LIVE) {
         vm->cycle_to_die -= CYCLE_DELTA;
+        vm->nbr_live = 0;
+    }
     if (vm->last_check + CYCLE_TO_DIE != vm->current_cycle)
         return (0);
     for (; champ; champ = champ->next) {
@@ -23,5 +25,6 @@ int check_alive_state(vm_t *vm)
             vm->nb_champ -= 1;
         }
     }
+    vm->last_check = vm->current_cycle;
     return (0);
 }
