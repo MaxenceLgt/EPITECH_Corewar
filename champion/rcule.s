6b89df03c95249386becec431444ddefffd00917
Augustin Bost <augustin.bost@epitech.eu>
2023-05-26 11:10:07 +0200

    UPDATE rcule work with live

--- champion/rcule.s
+++ champion/rcule.s
@@ -1,6 +1,56 @@
-.nom "Rcule"
-. commentaire "Cmoi le plu for"
-sti r1 , % : crow , %1
-crow : live %234
-ld %0 , r3
-zjmp % : crow
+.name "Rcule"
+.comment "Cmoa le plu phor"
+
+l2:		sti r1, %:live, %1
+		and r1, %0, r1
+
+
+live:
+live	%1
+live	%2
+live	%3
+live	%4
+live	%5
+live	%6
+live	%7
+live	%8
+live	%9
+live	%10
+live	%11
+live	%12
+live	%13
+live	%14
+live	%15
+live	%16
+live	%17
+live	%18
+live	%19
+live	%21
+live	%22
+live	%23
+live	%24
+live	%25
+live	%26
+live	%27
+live	%28
+live	%29
+live	%30
+live	%31
+live	%32
+live	%33
+live	%34
+live	%35
+live	%36
+live	%37
+live	%38
+live	%39
+live	%40
+live	%41
+live	%42
+live	%43
+live	%44
+live	%45
+
+live:	live %1
+		zjmp %:live
+
