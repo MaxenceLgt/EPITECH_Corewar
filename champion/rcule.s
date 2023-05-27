23587aa4782016f7daafdc81e354a3cc9fc80853
Augustin Bost <augustin.bost@epitech.eu>
2023-05-27 02:47:05 +0200

    refactor(rcule.s): update upgrade champion

--- champion/rcule.s
+++ champion/rcule.s
@@ -1,18 +1,39 @@
-.name "Rcule"
-.comment "Cmoa le plu phor"
+	.name "Rcule"
+	.comment "Cmoa le plu phor"
 
-l2:		sti r1, %:live, %1
-		and r1, %0, r1
+	sti r1, %:live, %1
+	and r1, %0, r1
+	fork %:init_forward
 
-live:	live %1
-		ld 1 r3
+	live:	live %1
+			zjmp %:live
+			fork %:attack
+
+
+	p1:
+			ld		%57672192,		r2
+			ld		1 	,			r5
+			ld		%318992387,		r3
+			ld		%0,				r4	# carry = 1
+	init_forward:
+			live	%42
+			fork	%:p1
+			live	%42
+			fork	%:attack
+			live	%42
+			fork	%:p1
+			live	%42
+			fork	%:attack
+			live	%42
+
+
+
+	attack:
+		st		r2,				15
+		st		r3,				-1
+		live	%1337
 		fork %:attack
-		zjmp %:live
 
-attack:
-	add r2, r2, r2
-	st r3, r2
-	zjmp %:live
 
 
 
